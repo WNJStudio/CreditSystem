51792e62213c3534b20db8cfbd36e06fadb229d7
WNJStudio <wnjstudio@outlook.com>
2019-05-15 15:35:19 +0200

    Added crawler baseline

--- index.js
+++ index.js
@@ -6,7 +6,35 @@ const app = express()
 const http = require('http').createServer(app)
 //socket module
 const io = require('socket.io')(http)
-// TODO: child processes with python
+//multithreading for python crawler
+const spawn = require('child_process').spawn;
+let crawler
+const startCrawler = rid =>{
+  if(!crawler){
+    crawler = spawn('python3',['crawler.py',rid],{detached:true})
+    console.log('Crawler running')
+  }else{
+    console.log('Crawler already running')
+  }
+}
+const stopCrawler = ()=>{
+  if(!crawler){
+    console.log('Crawler not running')
+  }else{
+    crawler.kill()
+    crawler = null
+    console.log('Killed Crawler')
+  }
+}
+//crawler data handling
+const handleCrawlerData = handler =>{
+  if(crawler){
+    crawler.stdout.on('data',handler)
+    console.log('Listening for data')
+  }else{
+    console.log('Crawler not running')
+  }
+}
 //linking static files
 app.use(express.static(path.resolve(__dirname,'client')))
 //webpage routing
@@ -17,7 +45,6 @@ app.get('*', (req, res) => {
 io.on('connection', socket => {
   console.log('a user connected')
 })
-
 //server listening
 http.listen(3000, () => {
   console.log('listening on *=3000')
