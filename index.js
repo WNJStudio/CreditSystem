b8e834a4337d34de8793cfa072257f53c721fae5
WNJStudio <wnjstudio@outlook.com>
2019-05-15 13:23:10 +0200

    Committing Codebase

---
+++ index.js
@@ -0,0 +1,24 @@
+//utilities
+const path = require('path');
+//webserver modules
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+//socket module
+const io = require('socket.io')(http)
+// TODO: child processes with python
+//linking static files
+app.use(express.static(path.join(__dirname,'client','scripts')))
+//webpage routing
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname,'client','index.html'))
+})
+//socket events routing
+io.on('connection', socket => {
+  console.log('a user connected')
+})
+
+//server listening
+http.listen(3000, () => {
+  console.log('listening on *=3000')
+})
